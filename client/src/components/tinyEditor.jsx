a303ab62f452cdab83b30d84f1c8b75bd0d888df
quipcode <abdulkarim8ahmed@gmail.com>
2021-07-27 22:40:47 -0700

    can say the editor is working as desired now

--- client/src/components/tinyEditor.jsx
+++ client/src/components/tinyEditor.jsx
@@ -1,166 +1,98 @@
 import { Editor } from '@tinymce/tinymce-react';
-import React, { useCallback, useState} from "react";
+import React, { useCallback, useState, useRef, useEffect} from "react";
+
 import constants from '../utils/constants';
 import StatusBar from './statusbar';
 import * as _ from "lodash";
-import { useFormik } from 'formik';
-// import { formik, Form, Field, ErrorMessage } from 'formik'
-import * as yup from 'yup'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import { green, orange, yellow } from '@material-ui/core/colors';
+
+function TinyEditor(props){
 
 
+    const [contentEditor, setContentEditor] = useState("");
+    const [contentTitle, setContentTitle] = useState("");
+    const [displayIsSaving, setDisplayIsSaving] = useState(false);
+    const [initContentEditor, setInitContentEditor] = useState("");
+    const [initContentTitle, setInitContentTitle] = useState("");
 
-// const validationSchema = yup.object({
-//     email: yup
-//         .string('Enter your email')
-//         .email('Enter a valid email')
-//         .required('Email is required'),
-//     password: yup
-//         .string('Enter your password')
-//         .min(8, 'Password should be of minimum 8 characters length')
-//         .required('Password is required'),
-// });
+    const handleEditorChange = (content, editor) => {
+        console.log('Content was updated:', content, editor);
+        setContentEditor(content);
+    }
 
-const validationSchema = yup.object({
-    contentTitlePresent: yup.boolean(),
-    contentTitle: yup.string().when("contentTitlePresent", {
-        is: true,
-        then: yup.string().required("Must provide content title to publish"),
-        otherwise: yup.string()
-    })
-})
-
-
-
-// class TinyEditor extends React.Component{
-function TinyEditor(){
-    // constructor(){
-    //     super();
-    //     this.state={
-    //         contentTitle: ""
-    //     }
-        
-    // }
-    // const [contentTitle, setContentTitle] = useState("");
-    // const [displayIsSaving, setDisplayisSaving] = useState(false);
-    // const [editorContent, setEditorContent] = useState("");
-
-    const [state, setState] = useState({ contentTitle: "", editorContent: "<p>hi</p>", displayIsSaving: false });
-    // const INITIAL_STATE = { a: false, b: false, c: false, d: false };
-    // const [state, setState] = useState(INITIAL_STATE);
-    // const [state, setstate] = useState({
-    //     Name: 'Hamza',
-    //     Status: 'Comitted'
-    // })
-
-
-    // function save(event) {
-        
-        
-    // }
-
-    function save(event) {
-        console.log("event is ")
+    const handleTitleChange = (event) => {
         console.log(event)
+        setContentTitle(event.target.value)
+    }
+
+
+
+    const handlePublish = (event) => {
+        setDisplayIsSaving(true)
+        throttledSaveToServer();
+        console.log("publish was pressed: ", contentEditor, contentTitle)
+        event.preventDefault();
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setDisplayIsSaving(true)
+        throttledSaveToServer();
+        console.log("handle submit was pressed: ", contentEditor, contentTitle)
         
-        // setState(prevState => ({ ...prevState, [name]: value }));
+        console.log("in handle submit")
+        console.log(event)
     }
 
-    const formik = useFormik({
-        initialValues: {
-            email: 'foobar@example.com',
-            password: 'foobar',
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
-
-
-    const handleChange = (e) => {
-      
-        const { name, value } = e.target
-        console.log(name, value)
-        setState(prevState => ({ ...prevState, [name]: value }));
+    const handleCancel = (event) => {
+        event.preventDefault();
+        console.log("cancel button")
+        console.log(event)
     }
-
-    console.log("love life")
-    console.log(state.editorContent)
-  
-
-    // save(newPartialState) {
-    //     this.setState({
-    //         ...newPartialState,
-    //         displayIsSaving: true
-    //     }, () => {
-    //         this.throttledSaveToServer();
-    //     });
-    // }
+    const editorRef = useRef(null);
 
     const throttledSaveToServer = _.throttle(() => {
         setTimeout(() => {
-            console.log("Saved to server", contentTitle, editorContent);
+            console.log("Saved to server", contentTitle, contentEditor);
             debouncedEndSaving();
         }, 500);
     }, 500);
 
     const debouncedEndSaving = _.debounce(() => {
-        setDisplayisSaving(true)
-        // this.setState({ displayIsSaving: false });
+        setDisplayIsSaving(false)
     }, 1000);
-    const handleEditorChange = (editorContent) => {
 
-        save({editorContent});
-    }
 
-    const handleNameChange = (contentTitle) => {
-        console.log("in handlenamechange and I am cotnentTitle = " + contentTitle)
-        if (contentTitle.length > 0) {
-            this.state.contentTitlePresent = true
-
-        }
-        this.save({ contentTitle });
-    }
     const componentWillUnmount = () => {
         debouncedEndSaving.cancel();
         throttledSaveToServer.cancel();
     }
 
-    const handleCancel = (e) =>{
-        e.preventDefault();
-        console.log("cancel button")
-        console.log(e)
-    }
-    const handleSave = (e) =>{
-        console.log("save buttons")
-        console.log(e)
-    }
-
     return (
         <div className="document-editor">
             <form>
+
+              
+
                 <TextField
                     fullWidth
                     id="contentTitle"
                     name="contentTitle"
                     label="Content Title"
-                    inputRef={(input) => save(input)}
-                    onChange={handleChange}
+                    value={contentTitle}
+                    onChange={handleTitleChange}
                 />
-               
                 <Editor
                     apiKey={constants.TINYMCE_API_KEY}
-                    initialValue={state.editorContent}
+                    //initialvalue must be some value...else cursor keeps resetting its position to begining of line on re-render...eyeroll...considered implementing tracking cursor location...but tinymce don't provide typical event.target.selectionstart associatied with events
+                    initialValue="<p>hi there</p>"
                     onInit={(evt, editor) => editorRef.current = editor}
                     init={{
                         selector: 'textarea#full-featured-non-premium',
                         plugins: 'save print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
                         imagetools_cors_hosts: ['picsum.photos'],
                         menubar: 'file edit view insert format tools table help',
-                        toolbar: 'save undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+                        toolbar: 'save undo redo | bold italic underline strikethrough  | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
                         toolbar_sticky: true,
                         autosave_ask_before_unload: true,
                         autosave_interval: '30s',
@@ -213,24 +145,26 @@ function TinyEditor(){
                         // skin: useDarkMode ? 'oxide-dark' : 'oxide',
                         // content_css: useDarkMode ? 'dark' : 'default',
                         content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
-                        save_onsavecallback: {handleSave}
+                        // save_onsavecallback: { handleEditorSave }
                     }}
-                    onChange={handleEditorChange}
-                    onSaveContent={handleSave}
+                    value={contentEditor}
+                    onEditorChange={handleEditorChange}
                 />
-           
-                <Button color="secondary" variant="contained" type="submit" disabled={!state.contentTitle} onClick={() => { alert('publish clicked') }}>
+
+               
+                <Button color="secondary" variant="contained" type="submit" disabled={!contentTitle} onClick={(e) => { handlePublish(e) }}>
                     Publish
                 </Button>
-                <Button color="primary" variant="contained" type="submit" onClick={() => { alert('submit clicked') }}>
+                <Button color="primary" variant="contained" type="submit" onClick={(e) => { handleSubmit(e) }} >
                     Save
                 </Button>
-                <Button color="default" variant="contained" type="reset" onClick={() => { alert('cancel clicked') }}>
+                <Button color="default" variant="contained" type="reset" onClick={(e) => { handleCancel(e) }}>
                     Cancel
                 </Button>
             </form>
         </div>
-    )
+    );
+    
 
 
 }
