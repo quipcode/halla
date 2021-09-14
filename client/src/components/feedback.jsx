db3684de808e363a1e3913ae91bf4c9f1089a918
quipcode <abdulkarim8ahmed@gmail.com>
2021-09-14 00:24:33 -0700

    working on feedback form...looks decent..need to work on validation

--- client/src/components/feedback.jsx
+++ client/src/components/feedback.jsx
@@ -5,7 +5,7 @@ import * as Yup from "yup";
 
 let feedbackTopic = [{ id: 0, name: null }, { id: 1, name: "Overall Site" }, { id: 2, name: "This Page" }, { id: 3, name: "Specific Feature" }]
 let feedbackSelectionGenerator = feedbackTopic.map(
-    ({ id, name }, key) => <option value={id} key={key}>{name}</option>
+    ({ id, name }, key) => key === 0 ? <option disabled defaultValue value={id} key={key}>Provide feedback on...</option> : <option value={id} key={key}>{name}</option> 
 )
 // const routeComponents = Object.values(routes).map(
 //     ({ path, component, privateRoute }, key) =>
@@ -34,28 +34,43 @@ const validationSchema = Yup.object({
 
 
 const MySelect = ({ label, ...props }) => {
-
     const [field, meta] = useField(props);
-
     return (
-
         <div>
-
-            <label htmlFor={props.id || props.name}>{label}</label>
-
-            <select {...field} {...props} />
-
+            {/* <label className="form-check-label" htmlFor={props.id || props.name}>{label}</label> */}
+            <select className={"form-select"} {...field} {...props} />
             {meta.touched && meta.error ? (
-
                 <div className="error">{meta.error}</div>
-
             ) : null}
-
         </div>
-
     );
-
 };
+
+const MyRadios = ({label, ...props}) => {
+    const [field, meta] = useField(props);
+    return(
+     
+        <div>
+            <label className="form-check-label" htmlFor={props.id || props.name}>{label}</label> 
+            <div className="form-check form-check-inline m-2">
+            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="asAnon" value="anon" defaultChecked/>
+            <label className="form-check-label" htmlFor="asAnon">Anonymous</label>
+            </div>
+            { props.user ? 
+            <div className="form-check form-check-inline">
+            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="asUser" value={props.user} />
+            <label className="form-check-label" htmlFor="asUser">As {props.user}</label>
+            </div> 
+            :    
+            <div className="form-check form-check-inline">
+            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="asUser" value="asUser" disabled/>
+            <label className="form-check-label" htmlFor="inlineRadio3">As User</label>
+            </div>}
+     
+         
+        </div> 
+    )
+}
 const FeedBack = (props) => {
 
 
@@ -82,9 +97,17 @@ const FeedBack = (props) => {
 
                             }}
                         >
+                            
                             <Form className="feedback-form">
+                            <h3> Feedback Form </h3>
+                            <fieldset>
+                            <div>
+                                {/* <label>Provide feedback as</label> */}
+                                    <MyRadios user={props.auth.username} label={"Provide feedback as: "}/>
+                                    </div>
+                                </fieldset>
                                 <fieldset className="form-group feedback_form_fields">
-                                    <MySelect label="Feedback On: " name="feedbackOn">
+                                    <MySelect   label="Feedback On: " name="feedbackOn">
                                         {feedbackSelectionGenerator}
 
                                     </MySelect>
@@ -94,9 +117,10 @@ const FeedBack = (props) => {
                                         </Field>
                                     </label> */}
                                 </fieldset>
+                           
                                 <fieldset className="form-group feedback_form_fields">
                                     {/* <label htmlFor="title">Title</label> */}
-                                    <Field name="title" placeholder="Title" />
+                                    <Field className={"form-control"} name="title" placeholder="Title" />
                                 </fieldset>
                                 {/* <div role="group" aria-labelledby="my-radio-group">
                                     <label>
@@ -108,24 +132,22 @@ const FeedBack = (props) => {
                                         Anonymous
                                     </label>
                                 </div> */}
-                                <div id="my-radio-group">Picked</div>
-                                <div role="group" aria-labelledby="my-radio-group">
-                                    <label>
-                                        <Field type="radio" name="asWhom" value={props.auth.username} />
-                                        One
-                                    </label>
-                                    <label>
-                                        <Field type="radio" name="asWhom" value="anon" />
-                                        Anonymous
-                                    </label>
-                                    {/* <div>Picked: {values.picked}</div> */}
-                                </div>
-                                <fieldset>
-                                    <label htmlFor="feedback">FeedBack</label>
-                                    <Field name="feedback" component="textarea" rows="7" ></Field>
+                          
+                    
+                               <div>
+                               <fieldset>
+                                    {/* <label htmlFor="feedback">FeedBack</label> */}
+                                    {/* <Field name="feedback" component="textarea" rows="7" ></Field> */}
+                                    <Field placeholder={"Feedback"} className={'form-control' } name="feedback" component="textarea" rows="7" />
                                 </fieldset>
-                                <button type="button" className="btn btn-danger">Cancel</button>
-                                <button className="btn btn-primary" type="submit">Submit</button>
+                                   </div>
+                                
+                                    
+                                    <div className="text-center" role="toolbar" aria-label="Toolbar with button groups">
+                                        <div className="btn-group m-2" role="group" aria-label="Second group"><button type="button" className="btn btn-danger">Cancel</button></div>
+                                        <div className="btn-group" role="group" aria-label="Third group"><button className="btn btn-primary" type="submit">Submit</button></div>
+                                    </div>
+                                
                                 <hr className="my-4" />
                             </Form>
                         </Formik>
