a2e7f2473ceea9cc70cad04eb112d18b71ace3d6
quipcode <abdulkarim8ahmed@gmail.com>
2021-09-14 18:37:53 -0700

    this is still very messy but the radio buttons allowing users to specify anonymous or as self feedback is semi-working...thanks to morioh https://morioh.com/p/bdca60be4f18 specifically his formik control demo see https://github.com/gopinav/React-Formik-Tutorials/tree/master/formik-controls-demo

--- client/src/components/feedback.jsx
+++ client/src/components/feedback.jsx
@@ -15,20 +15,16 @@ let feedbackSelectionGenerator = feedbackTopic.map(
 // );
 
 const validationSchema = Yup.object({
-    feedback: Yup.string().required("Required"),
-    title: Yup.string().required("Required"),
+    feedback: Yup.string().required("Feedback is required"),
+    title: Yup.string().required("Title is required"),
 
     feedbackOn: Yup.string()
-
         .oneOf(
-
             ['1', '2', '3'],
-
-            'Invalid Job Type'
-
+            'Please specify feedback scope'
         )
-
         .required('Required'),
+    asWhom: Yup.string().required("")
 
 })
 
@@ -36,11 +32,19 @@ const validationSchema = Yup.object({
 const MySelect = ({ label, ...props }) => {
     const [field, meta] = useField(props);
     return (
+        
         <div>
+            
             {/* <label className="form-check-label" htmlFor={props.id || props.name}>{label}</label> */}
             <select className={"form-select"} {...field} {...props} />
             {meta.touched && meta.error ? (
-                <div className="error">{meta.error}</div>
+                
+                 <ErrorMessage name={field.name} component="div" className="invalid-feedback">{meta.error}</ErrorMessage>
+                // <div className={'form-control is-invalid'}>
+                    
+                //     {meta.error}
+                // </div>
+                // <div className="error">{meta.error}</div>
             ) : null}
         </div>
     );
@@ -51,28 +55,91 @@ const MyRadios = ({label, ...props}) => {
     return(
      
         <div>
+            
             <label className="form-check-label" htmlFor={props.id || props.name}>{label}</label> 
             <div className="form-check form-check-inline m-2">
-            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="asAnon" value="anon" defaultChecked/>
+                <input className="form-check-input" type="radio" name="inlineRadioOptions"  {...field} {...props} id="asAnon" value="anon" defaultChecked/>
             <label className="form-check-label" htmlFor="asAnon">Anonymous</label>
             </div>
             { props.user ? 
+             
             <div className="form-check form-check-inline">
-            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="asUser" value={props.user} />
-            <label className="form-check-label" htmlFor="asUser">As {props.user}</label>
-            </div> 
-            :    
-            <div className="form-check form-check-inline">
-            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="asUser" value="asUser" disabled/>
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions"   id="asUser" value="asUser" disabled/>
             <label className="form-check-label" htmlFor="inlineRadio3">As User</label>
-            </div>}
+            </div>
+            :
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions"   id="asUser" value="15" />
+                    <label className="form-check-label" htmlFor="asUser">As {props.user}</label>
+                </div> 
+            
+            }
      
          
         </div> 
     )
 }
+
+
+
+const  RadioButtons = (props) => {
+    const { label, name, options, user, ...rest } = props
+    return (
+        <div className='form-control'>
+            <label>{label}: </label>
+            <Field name={name} >
+                {({ field }) => {
+                    return options.map(option => {
+                        
+                        return (
+                            
+                            <React.Fragment key={option.key} >
+                                {option.value == "asUser" ?
+                                    <input
+                                        type='radio'
+                                        id={option.value}
+                                        {...field}
+                                        {...rest}
+                                        value={option.value}
+                                        checked={field.value === option.value}
+                                        disabled
+                                    /> :
+                                    <input
+                                        type='radio'
+                                        id={option.value}
+                                        {...field}
+                                        {...rest}
+                                        value={option.value}
+                                        checked={field.value === option.value}
+                                    />
+
+                                }
+                               
+                                <label htmlFor={option.value}>{option.key}</label>
+
+            
+                            </React.Fragment>
+                        )}
+
+                    )
+                }}
+            </Field>
+            <ErrorMessage component="div" name={name} />
+        </div>
+    )
+}
+
+
+
 const FeedBack = (props) => {
 
+    const radioOptions = props.auth.username ? [
+        { key: 'Anonymous', value: 'anon' },
+        { key: "As " + props.auth.username, value: props.auth.username },
+    ] : [
+        { key: 'Anonymous', value: 'anon' },
+        { key: 'As User', value: 'asUser' },
+    ]
 
     return (
         
@@ -82,7 +149,7 @@ const FeedBack = (props) => {
                 <div className="card">
                     <article className="card-body">
                         <Formik
-                            initialValues={{ title: '', feedback: '', feedbackOn: "0", asWhom:"" }}
+                            initialValues={{ title: '', feedback: '', feedbackOn: "0", asWhom:"anon" }}
                             onSubmit={(values, { setSubmitting }) => {
                                 alert(JSON.stringify(values, null, 2));
                                 setSubmitting(false);
@@ -96,21 +163,40 @@ const FeedBack = (props) => {
                                 // }, 400);
 
                             }}
+                            validationSchema={validationSchema}
                         >
-                            
+                            {
+                                ({ errors, touched }) => (
                             <Form className="feedback-form">
                             <h3> Feedback Form </h3>
                             <fieldset>
                             <div>
                                 {/* <label>Provide feedback as</label> */}
-                                    <MyRadios user={props.auth.username} label={"Provide feedback as: "}/>
+                                    <MyRadios user={props.auth.username} label={"Provide feedback as: "} name="asWhom" />
+                                    </div>
+                                    {/* <div>
+                                                <MySpecialFieldHook/>
+                                    </div>
+                                    <div>
+                                                <MySpecialFieldHook2 name="asWhom"> 
+                                                    <input className={"form-select"} className="mr-2 leading-tight" type="radio" value="15" />
+                                                    <input className={"form-select"} className="mr-2 leading-tight" type="radio" value="16" />
+                                                    <input className={"form-select"} className="mr-2 leading-tight" type="radio" value="17" />
+                                                    
+                                                </MySpecialFieldHook2>
+                                    </div> */}
+                                    <div>
+                                                <RadioButtons label={"Provide feedback as"} options={radioOptions} name={"asWhom"} user={"bob"}/>
                                     </div>
                                 </fieldset>
                                 <fieldset className="form-group feedback_form_fields">
-                                    <MySelect   label="Feedback On: " name="feedbackOn">
+                                            
+                                            <MySelect className={'form-control' + (errors.feedbackOn && touched.feedbackOn ? ' is-invalid' : '')}  label="Feedback On: " name="feedbackOn">
+                                                
                                         {feedbackSelectionGenerator}
 
                                     </MySelect>
+                                            <ErrorMessage name="feedbackOn" component="div" className="invalid-feedback" />
                                     {/* <label htmlFor="feedbackOn">Feedback On:
                                         <Field name="feedbackOn" component="select">
                                             {feedbackSelectionGenerator}
@@ -120,7 +206,10 @@ const FeedBack = (props) => {
                            
                                 <fieldset className="form-group feedback_form_fields">
                                     {/* <label htmlFor="title">Title</label> */}
-                                    <Field className={"form-control"} name="title" placeholder="Title" />
+                                           
+                                    {/* <Field className={"form-control"} name="title" placeholder="Title" /> */}
+                                            <Field className={'form-control' + (errors.title && touched.title ? ' is-invalid' : '')}  name="title" placeholder="Title" />
+                                            <ErrorMessage name="title" component="div" className="invalid-feedback" />
                                 </fieldset>
                                 {/* <div role="group" aria-labelledby="my-radio-group">
                                     <label>
@@ -138,7 +227,8 @@ const FeedBack = (props) => {
                                <fieldset>
                                     {/* <label htmlFor="feedback">FeedBack</label> */}
                                     {/* <Field name="feedback" component="textarea" rows="7" ></Field> */}
-                                    <Field placeholder={"Feedback"} className={'form-control' } name="feedback" component="textarea" rows="7" />
+                                                <Field placeholder={"Feedback"} t className={'form-control' + (errors.feedback && touched.feedback ? ' is-invalid' : '')} name="feedback" component="textarea" rows="7" />
+                                                <ErrorMessage name="feedback" component="div" className="invalid-feedback" />
                                 </fieldset>
                                    </div>
                                 
@@ -150,6 +240,8 @@ const FeedBack = (props) => {
                                 
                                 <hr className="my-4" />
                             </Form>
+                                )
+                            }
                         </Formik>
                     </article>
                 </div>
