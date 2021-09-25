2aaa0d7f7fff1aafe8637c8957bb25caba51804d
quipcode <abdulkarim8ahmed@gmail.com>
2021-09-24 17:25:34 -0700

    finally after nearly a whole week...realized how to persist state...you use local storage...by design redux will not maintain state and on page refresh state will be lost...so you have to pull from local storage...see dan abramov's video on persisting to local storage https://egghead.io/lessons/javascript-redux-persisting-the-state-to-the-local-storage associated code repo https://github.com/gaearon/todos/tree/03-persisting-state-to-local-storage

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -2,7 +2,10 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { routerMiddleware } from 'connected-react-router';
 import rootReducer from './root-reducer'
 import thunk from 'redux-thunk'
+import { loadState, saveState } from './localStorage';
+import { throttle } from 'lodash';
 
+const persistedState = loadState();
 export default function configureStore(initialState = {}, history){
     let composeEnhancers = compose;
     
@@ -12,10 +15,15 @@ export default function configureStore(initialState = {}, history){
 
     const store = createStore(
         rootReducer(),
-        initialState,
+        persistedState,
         composeEnhancers(...enhancers),
     );
 
+    store.subscribe(throttle(() => {
+        saveState({
+            auth: store.getState().auth,
+        });
+    }, 1000))
     
     store.injectedReducers = {};
     
