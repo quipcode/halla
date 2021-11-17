a4af307aa271b5d7c55fed3ae91a846b36decfcf
quipcode <abdulkarim8ahmed@gmail.com>
2021-11-16 23:23:30 -0800

    refactoring to microfrontend + typescript

---
+++ frontend/rima/webpack.config.ts
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// import MiniCssExtractPlugin from "mini-css-extract-plugin"
+// import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const htmlWebPackPlugin = new HtmlWebpackPlugin({
+    template: path.join(__dirname, 'src', 'index.html')
+})
+
+module.exports = {
+    mode: 'none',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    target: 'web',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.(sass|scss|css)$/,
+                use: [
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+
+                ]
+            },
+
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html')
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, "dist"),
+        compress: true,
+        port: 4000,
+    },
+}
